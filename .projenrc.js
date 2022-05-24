10bf4313b9be3aac482d2366349a5657cb672f64
Daniel von Eßen <daniel@vonessen.eu>
2022-05-24 07:03:09 +0200

    chore: :wrench: Changed documentation path to documentation/API.md

--- .projenrc.js
+++ .projenrc.js
@@ -1,4 +1,11 @@
+const fs = require('fs');
+const path = require('path');
 const { cdk } = require('projen');
+
+const docPath = './documentation';
+if (fs.existsSync(docPath) == false) {
+  fs.mkdirSync(docPath, { recursive: false, mode: 0o750 });
+}
 
 const project = new cdk.JsiiProject({
   author: 'Daniel von Eßen',
@@ -8,6 +15,8 @@ const project = new cdk.JsiiProject({
   repositoryUrl: 'https://gitlab.com/dvonessen/gcix.git',
   packageName: 'gcix', /* The "name" in package.json. */
   description: 'GitLab CI multi language Library (x)', /* The description is just a string that helps people understand the purpose of the package. */
+  docgen: true,
+  docgenFilePath: path.join(docPath, 'API.md'),
   github: false,
 });
 
