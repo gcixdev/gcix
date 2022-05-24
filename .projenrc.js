0c0e24bf85eb136eb11ded0647cd968c06cc7f4d
Daniel von Eßen <daniel@vonessen.eu>
2022-05-24 06:59:28 +0200

    build(projen): :wrench: Disabled Github integration. Added correct project information.

--- .projenrc.js
+++ .projenrc.js
@@ -1,14 +1,14 @@
 const { cdk } = require('projen');
+
 const project = new cdk.JsiiProject({
   author: 'Daniel von Eßen',
   authorAddress: 'daniel@vonessen.eu',
   defaultReleaseBranch: 'main',
   name: 'gcix',
-  repositoryUrl: 'https://github.com/daniel/gcix.git',
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  repositoryUrl: 'https://gitlab.com/dvonessen/gcix.git',
+  packageName: 'gcix', /* The "name" in package.json. */
+  description: 'GitLab CI multi language Library (x)', /* The description is just a string that helps people understand the purpose of the package. */
+  github: false,
 });
+
 project.synth();
