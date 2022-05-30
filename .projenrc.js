2f8486ab9fb6e77fc55a9dae8aa4e9f96045b26e
Daniel von Eßen <daniel@vonessen.eu>
2022-05-30 07:24:22 +0200

    chore: :wrench: Removed custom documentation directory.

--- .projenrc.js
+++ .projenrc.js
@@ -1,11 +1,4 @@
-const fs = require('fs');
-const path = require('path');
 const { cdk } = require('projen');
-
-const docPath = './documentation';
-if (fs.existsSync(docPath) == false) {
-  fs.mkdirSync(docPath, { recursive: false, mode: 0o750 });
-}
 
 const project = new cdk.JsiiProject({
   author: 'Daniel von Eßen',
@@ -13,10 +6,10 @@ const project = new cdk.JsiiProject({
   defaultReleaseBranch: 'main',
   name: 'gcix',
   repositoryUrl: 'https://gitlab.com/dvonessen/gcix.git',
-  packageName: 'gcix', /* The "name" in package.json. */
-  description: 'GitLab CI multi language Library (x)', /* The description is just a string that helps people understand the purpose of the package. */
+  packageName: 'gcix', // The "name" in package.json.
+  description:
+    'GitLab CI multi language Library (x)',
   docgen: true,
-  docgenFilePath: path.join(docPath, 'API.md'),
   github: false,
 });
 
