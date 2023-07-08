4bf45300dbd2bbd0655f0fd5559d0f4a7c0cd4cb
Daniel von Essen <daniel@vonessen.eu>
2023-07-08 15:45:19 +0200

    chore(dependency): :heavy_plus_sign: Added ts-node as a devDependency.

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,30 +1,31 @@
-import { cdk } from "projen";
+import { cdk } from 'projen';
 
 const gcixProject = new cdk.JsiiProject({
-  author: "Daniel von Essen",
-  copyrightOwner: "Daniel von Essen",
-  authorAddress: "daniel@vonessen.eu",
+  author: 'Daniel von Essen',
+  copyrightOwner: 'Daniel von Essen',
+  authorAddress: 'daniel@vonessen.eu',
   description: 'GitLab CI X Library (X stands for multilanguage)',
-  defaultReleaseBranch: "main",
-  jsiiVersion: "~5.0.0",
-  name: "gcix",
+  defaultReleaseBranch: 'main',
+  jsiiVersion: '~5.0.0',
+  name: 'gcix',
   projenrcTs: true,
-  repositoryUrl: "https://gitlab.com/gcix/gcix.git",
+  repositoryUrl: 'https://gitlab.com/gcix/gcix.git',
   docgen: true,
   github: false,
   bundledDeps: [
-    "js-yaml"
+    'js-yaml',
   ],
   devDeps: [
     'sanitize-filename',
+    'ts-node',
     '@types/js-yaml',
     '@types/node',
     '@types/jest',
   ],
-  docsDirectory: "public",
+  docsDirectory: 'public',
   publishToPypi: {
-    distName: "gcix",
-    module: "gcix",
+    distName: 'gcix',
+    module: 'gcix',
   },
   vscode: true,
 });
