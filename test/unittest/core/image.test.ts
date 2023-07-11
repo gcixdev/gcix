34d2ab5addbcc7fdbc0b9581b8360ba2e2a7bf43
Daniel von Essen <daniel@vonessen.eu>
2023-07-11 10:22:05 +0200

    test: :white_check_mark: Added tests for Image class.

--- test/unittest/core/image.test.ts
+++ test/unittest/core/image.test.ts
@@ -1,35 +1,42 @@
-import { Image } from '../../../src/core/image';
+import { Image } from '../../../src/';
 
-describe('image', () => {
-  test('image class only with image', () => {
-    const image = new Image({
-      name: 'alpine:3',
-    });
-    expect(image.name).toBe('alpine:3');
-    expect(image.entrypoint).toBe(undefined);
-    expect(image.render()).toMatchObject({
-      name: 'alpine:3',
-    });
+test('image class only with image', () => {
+  const image = new Image({
+    name: 'alpine:3',
   });
-  test('image class with entrypoint', () => {
-    const image = new Image({
-      name: 'alpine:3', entrypoint: ['/bin/sh', '-c', 'cat', '/etc/os-release'],
-    });
-    expect(image.name).toBe('alpine:3');
-    expect(image.entrypoint).toMatchObject(['/bin/sh', '-c', 'cat', '/etc/os-release']);
-    expect(image.render()).toMatchObject({ name: 'alpine:3', entrypoint: ['/bin/sh', '-c', 'cat', '/etc/os-release'] });
+  expect(image.name).toBe('alpine:3');
+  expect(image.entrypoint).toBe(undefined);
+  expect(image.render()).toMatchObject({
+    name: 'alpine:3',
   });
 });
 
-/**
- * @ToDo
- * Missing PredefinedImages class
- */
-// def test_image_with_tag():
-//     assert PredefinedImages.GCIP.with_tag("otherThanLatest").tag == "otherThanLatest"
-//     assert PredefinedImages.GCIP.tag == "latest"
+test('image class with entrypoint', () => {
+  const image = new Image({
+    name: 'alpine:3', entrypoint: ['/bin/sh', '-c', 'cat', '/etc/os-release'],
+  });
+  expect(image.name).toBe('alpine:3');
+  expect(image.entrypoint).toMatchObject(['/bin/sh', '-c', 'cat', '/etc/os-release']);
+  expect(image.render()).toMatchObject({ name: 'alpine:3', entrypoint: ['/bin/sh', '-c', 'cat', '/etc/os-release'] });
+});
 
+describe('image with', () => {
+  const image = new Image({ name: 'busybox' });
+  test('tag', () => {
+    const imageWithTag = image.withTag('1.2.3');
+    expect(imageWithTag.tag).toBe('1.2.3');
+  });
+  test('entrypoint', () => {
+    const imageWithEntrypoint = image.withEntrypoint(['/bin/sh', '-c', 'echo "Hello world!']);
+    expect(imageWithEntrypoint.entrypoint).toEqual(['/bin/sh', '-c', 'echo "Hello world!']);
+  });
+});
 
-// def test_image_with_entrypoint():
-//     assert PredefinedImages.GCIP.with_entrypoint("foo", "bar").entrypoint == ["foo", "bar"]
-//     assert PredefinedImages.GCIP.entrypoint is None
+test('equality', () => {
+  const imageToCompareTo = new Image({ name: 'busybox', entrypoint: ['/bin/bash', '-c', 'true'], tag: 'latest' });
+  const imageEqualsToCompareTo = new Image({ name: 'busybox', entrypoint: ['/bin/bash', '-c', 'true'], tag: 'latest' });
+  const imageNotEqualsToCompareTo = new Image({ name: 'busybox', entrypoint: ['/bin/bash', '-c', 'true'], tag: 'v1.1.1' });
+
+  expect(imageToCompareTo.isEqual(imageEqualsToCompareTo)).toBe(true);
+  expect(imageToCompareTo.isEqual(imageNotEqualsToCompareTo)).toBe(false);
+});
