64c6a21d376813dac299bf7153d6d3e0d63648a2
Daniel von Essen <daniel@vonessen.eu>
2023-07-12 12:22:57 +0200

    test: :white_check_mark: Added test for OrderedStringSet

---
+++ test/unittest/core/base.test.ts
@@ -0,0 +1,39 @@
+import { OrderedStringSet } from '../../../src';
+test('OrderedStringSet', () => {
+  const orderedStringSet = new OrderedStringSet();
+
+  // Test adding values
+  orderedStringSet.add('foo');
+  expect(orderedStringSet.size).toBe(1);
+  expect(orderedStringSet.has('foo')).toBeTruthy();
+  expect(orderedStringSet.values).toEqual(['foo']);
+
+  orderedStringSet.add('bar');
+  expect(orderedStringSet.size).toBe(2);
+  expect(orderedStringSet.has('bar')).toBeTruthy();
+  expect(orderedStringSet.values).toEqual(['foo', 'bar']);
+
+  orderedStringSet.add(['hello', 'world']);
+  expect(orderedStringSet.has('world')).toBeTruthy();
+  expect(orderedStringSet.size).toBe(4);
+  expect(orderedStringSet.values).toEqual(['foo', 'bar', 'hello', 'world']);
+
+  orderedStringSet.delete('hello');
+  expect(orderedStringSet.has('hello')).not.toBeTruthy();
+  expect(orderedStringSet.size).toBe(3);
+  expect(orderedStringSet.values).toEqual(['foo', 'bar', 'world']);
+
+  orderedStringSet.clear();
+  expect(orderedStringSet.size).toBe(0);
+  expect(orderedStringSet.values).toEqual([]);
+});
+
+test('iterating over ordered string set', () => {
+  const initValues = ['foo', 'bar', 'baz', 'hello', 'world'];
+  const orderedStringSet = new OrderedStringSet(initValues);
+
+  for (let [key, value] of initValues.entries()) {
+    expect(initValues[key]).toBe(value);
+  }
+
+});
