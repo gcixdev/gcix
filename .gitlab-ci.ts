9b975d0302d3692a3873e9404b7b9ed5ab0964b7
Daniel von Essen <daniel@vonessen.eu>
2023-07-21 09:14:00 +0200

    ci: :green_heart: Added python3-venv to package job.

--- .gitlab-ci.ts
+++ .gitlab-ci.ts
@@ -42,7 +42,7 @@ testCollection.prependScripts(["npx projen install:ci"]);
 
 const packageJob = new Job({
   scripts: [
-    "apt update && apt install -y python3-pip",
+    "apt update && apt install -y python3-pip python3-venv",
     "npx projen install:ci",
     "npx projen package-all",
   ],
