eb399ea581255d7d7993bca18936208845a0b69e
Daniel von Essen <daniel@vonessen.eu>
2023-07-24 13:58:37 +0200

    ci: :construction_worker: Added better ci handling for different branches and tags.

--- .gitlab-ci.ts
+++ .gitlab-ci.ts
@@ -9,58 +9,71 @@ import {
 const pipeline = new Pipeline();
 
 const lintJob = new Job({
-  scripts: ["npx projen eslint"],
+  scripts: ["npx projen ci:lint"],
   name: "lint",
   stage: "test",
 });
 const testJob = new Job({
-  scripts: ["npx projen test"],
+  scripts: ["npx projen ci:test"],
   artifacts: new Artifacts({
     reports: [{ reportType: "junit", file: "test-reports/junit.xml" }],
   }),
-  name: "test-jest",
+  name: "jest",
   stage: "test",
 });
 const testCollection = new JobCollection();
 testCollection.addChildren({
   jobsOrJobCollections: [lintJob, testJob],
 });
-testCollection.initializeImage("node:18");
-testCollection.prependScripts(["npx projen install:ci"]);
-
-const packageJob = new Job({
-  scripts: ["npx projen ci:package"],
-  image: "node:18",
-  artifacts: new Artifacts({
-    paths: ["lib", ".jsii", "tsconfig.json", "dist/"],
-  }),
-  name: "package-all",
-  stage: "package",
-});
 
 pipeline.addChildren({
-  jobsOrJobCollections: [testCollection, packageJob],
+  jobsOrJobCollections: [testCollection],
 });
+
+if (
+  PredefinedVariables.ciCommitBranch === PredefinedVariables.ciDefaultBranch
+) {
+  pipeline.addChildren({
+    jobsOrJobCollections: [
+      new Job({
+        scripts: [
+          "npx projen ci:setup:git",
+          `cd ${PredefinedVariables.ciCommitShortSha}`,
+          "npx projen ci:publish:git",
+        ],
+        artifacts: new Artifacts({
+          paths: ["lib", ".jsii", "tsconfig.json", "dist/"],
+        }),
+        name: "publish-git",
+        stage: "publish",
+      }),
+    ],
+  });
+}
 
 /**
  * Only add publishCollection if pipeline is running on a tag.
  */
 if (PredefinedVariables.ciCommitTag) {
+  const packageJob = new Job({
+    scripts: ["npx projen ci:package-all"],
+    name: "package",
+    stage: "package",
+    artifacts: new Artifacts({
+      paths: ["lib", ".jsii", "tsconfig.json", "dist/"],
+    }),
+  });
+  const publishJob = new Job({
+    scripts: ["npx projen ci:publish-all"],
+    name: "npm",
+    stage: "publish",
+  }).addNeeds([packageJob]);
+
   pipeline.addChildren({
-    jobsOrJobCollections: [
-      new Job({
-        scripts: ["npx projen ci:publish-npmjs"],
-        name: "npm",
-        stage: "publish",
-      }).addNeeds([packageJob]),
-      new Job({
-        scripts: ["npx projen ci:publish-pypi"],
-        name: "pypi",
-        stage: "publish",
-      }).addNeeds([packageJob]),
-    ],
+    jobsOrJobCollections: [packageJob, publishJob],
   });
 }
 
+pipeline.initializeImage("node:18");
 pipeline.addTags(["gcix"]);
 pipeline.writeYaml();
