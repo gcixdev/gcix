c9771db1a11d286ffcc1b8ee8579a2d66ed458f5
Daniel von Essen <daniel@vonessen.eu>
2023-09-08 12:16:28 +0200

    test(addons): :white_check_mark: Add tests for container BuildContainerCollection class.

---
+++ test/unittest/addons/container/build-collections.test.ts
@@ -0,0 +1,41 @@
+import { Pipeline } from "../../../../src";
+import {
+  DockerClientConfig,
+  Registry,
+  BuildContainerCollection,
+} from "../../../../src/container";
+import { check } from "../../../comparison";
+
+let pipeline: Pipeline;
+let dcc: DockerClientConfig;
+const OLD_ENV = process.env;
+beforeEach(() => {
+  pipeline = new Pipeline();
+  dcc = new DockerClientConfig();
+  process.env = { ...OLD_ENV };
+});
+
+test("build container collection", () => {
+  dcc.addCredHelper(Registry.QUAY, "quay-login");
+  pipeline.addChildren({
+    jobsOrJobCollections: [
+      new BuildContainerCollection({
+        dockerClientConfig: dcc,
+        registry: Registry.QUAY,
+      }),
+    ],
+  });
+  check(pipeline.render(), expect);
+});
+
+test("build container collection with customized jobs", () => {
+  dcc.addAuth(Registry.DOCKER);
+  const fcs = new BuildContainerCollection({
+    registry: Registry.DOCKER,
+    dockerClientConfig: dcc,
+  });
+  fcs.trivyScanLocalImageJob.assignImage("custom/trivy:v1.2.3");
+  fcs.kanikoExecuteJob.buildArgs = { first_arg: "foo", second_arg: "bar" };
+  pipeline.addChildren({ jobsOrJobCollections: [fcs] });
+  check(pipeline.render(), expect);
+});
